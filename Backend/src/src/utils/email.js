18df576602803196655249f0b9078fc3c3d98250
Giani2011 <123737458+Giani2011@users.noreply.github.com>
2025-10-03 11:46:18 +0200

    Delete Backend directory

--- Backend/src/src/utils/email.js
+++
@@ -1,6 +0,0 @@
-// placeholder email helper. Swap in nodemailer or any provider later.
-async function sendEmail(to, subject, text) {
-  console.log('sendEmail called', { to, subject, text });
-  // IMPLEMENTATION: use nodemailer / SendGrid / SES in production
-}
-module.exports = { sendEmail };
